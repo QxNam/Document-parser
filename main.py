863a6224e0e165e6716570a1bec56d72aff48c54
zhung <caonguyengiahung442@gmail.com>
2026-01-02 21:02:54 +0700

    add logging functionality to main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("hello world")
+from dpe.configs.logger import get_logger
+
+logger = get_logger("log_from_main")
+
+
+if __name__ == "__main__":
+    logger.debug("This is a debug message from main.py")
